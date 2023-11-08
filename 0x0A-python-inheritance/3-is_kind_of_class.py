2ef1141967500f5b61650ecd387db643cc6f24db
ABRARgomaa <abrargomaa111@gmail.com>
2023-11-07 17:54:09 -0800

    H

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module for alx task"""
+
+
+def is_kind_of_class(obj, a_class):
+    """method for alx task
+
+     Args:
+     obj
+     a_class
+     """
+     if isinstance(obj, a_class):
+         return True
+     else:
+         return False
