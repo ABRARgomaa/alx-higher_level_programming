8cdf351eee3887294a83d757a16d9bb32a587381
ABRARgomaa <abrargomaa111@gmail.com>
2023-11-08 17:17:53 -0800

    h

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module for alx task
+"""
+
+
+class MyInt(int):
+    """child class Myint
+    Args:
+    int (class): parent class
+    """
+
+    def __eq__(self, value):
+        return self.real != value
+    def __ne__(self, value):
+        return self.real == value
