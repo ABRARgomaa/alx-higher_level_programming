7392fb51056659f1985311546431c5889ae1f343
ABRARgomaa <abrargomaa111@gmail.com>
2024-01-30 03:04:30 +0200

    hi

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script that fetches
+https://alx-intranet.hbtn.io/sta
+
+"""
+
+if __name__ == "__main__":
+    import urllib.request
+    u = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(u)as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode('utf-8'))
